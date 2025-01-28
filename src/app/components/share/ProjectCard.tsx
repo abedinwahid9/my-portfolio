4e24a0d1c0b90d2e271505cdb006d567fa6fa025
abedin wahid <abedinwahid9@gmail.com>
2025-01-29 02:26:33 +0600

    card design complete

--- src/app/components/share/ProjectCard.tsx
+++ src/app/components/share/ProjectCard.tsx
@@ -1,28 +1,134 @@
 "use client";
 import { motion } from "framer-motion";
-import React from "react";
+import Image from "next/image";
+import React, { useState } from "react";
+import demo from "@/assets/demo.png";
+import html from "../../../../public/icons/icons8-html-480.png";
+import css from "../../../../public/icons/icons8-css3-480.png";
+import bootstrap from "../../../../public/icons/icons8-bootstrap-480.png";
+import express from "../../../../public/icons/icons8-express-js-500.png";
+import tailwind from "../../../../public/icons/icons8-tailwindcss-480.png";
+import mongoose from "../../../../public/icons/icons8-mongoose-480.png";
 
 const ProjectCard: React.FC = () => {
+  const [hover, setHover] = useState<boolean>(false);
+
+  const description: string = `   Excited to share my latest project: This product has used Html, Css,
+            Css framework Tailwind css, Vanilla javascript. This website fully
+            responsive tablet and mobile screen`;
+
+  const cardVariants = {
+    // default: { x: 10, zIndex: 1 },
+    start: {
+      x: [10, 60, 10], // Moves right on hover
+      transition: {
+        duration: 1,
+        ease: "easeIn",
+      },
+      zIndex: 9999,
+    },
+    animate: {
+      x: [10, 65, 10],
+      zIndex: 1,
+      transition: {
+        duration: 1,
+        ease: "easeOut",
+      },
+    },
+  };
+
   return (
-    <motion.div className="sm:w-[300px] sm:h-[400px] w-[280px] h-[300px] rounded-3xl bg-[#50aab86e] relative z-0 shadow-lg">
+    <motion.div
+      onHoverStart={() => {
+        setHover(true);
+      }}
+      onHoverEnd={() => {
+        setHover(false);
+      }}
+      className="sm:w-[300px] h-[400px] w-[280px]  rounded-3xl bg-[#50aab86e] relative z-0 shadow-lg"
+    >
       {/* Card right bar */}
-      <div className="sm:w-[60px] sm:h-[320px] w-[40px] h-[220px] bg-[#50aab86e] absolute top-5 left-0 z-40 rounded-r-full drop-shadow-2xl"></div>
+      <div className="sm:w-[60px] h-[320px] w-[40px]  bg-[#50aab86e] absolute top-5 left-0 z-40 rounded-r-full drop-shadow-2xl"></div>
       <motion.div
-        initial={{ x: 10, zIndex: 1 }} // Initial position
-        whileHover={{
-          x: [10, 100, 10], // Moves right on hover
-          transition: {
-            duration: 0.5,
-            ease: "easeInOut",
-            delay: 0.1,
-          },
-          zIndex: 9999,
-        }}
-        animate={{ x: [10, 100, 10], zIndex: 1 }} // Returns to initial position after hover
-        className="sm:w-[300px] sm:h-[340px] w-[280px] h-[240px] bg-dr-link absolute top-3 -right-1 rounded-2xl drop-shadow-2xl"
-      ></motion.div>
+        variants={cardVariants}
+        // initial="default"
+        animate={hover ? "start" : "animate"}
+        className="sm:w-[300px] h-[340px] w-[280px]  
+     bg-gradient-to-bl from-lg-button to-dr-link  backdrop-blur-3xl
+
+
+ border-dr-link border-2  
+ absolute p-2 top-3 -right-1 rounded-2xl drop-shadow-2xl"
+      >
+        <Image
+          className="w-full h-3/5 object-cover  rounded-t-xl overflow-hidden"
+          src={demo}
+          alt="demo"
+          width={0}
+          height={0}
+        />
+        <div className="pt-2 flex flex-col gap-2">
+          <p className="text-dr-text font-semibold text-sm capitalize">
+            {description?.slice(0, 90)}...
+          </p>
+          <p className="text-dr-text font-bold text-base capitalize flex items-center gap-2">
+            <span>front end:</span>{" "}
+            <div className="flex">
+              <Image
+                className="w-6 h-6"
+                src={html}
+                alt="html"
+                width={0}
+                height={0}
+              />
+              <Image
+                className="w-6 h-6"
+                src={css}
+                alt="html"
+                width={0}
+                height={0}
+              />
+              <Image
+                className="w-6 h-6"
+                src={bootstrap}
+                alt="html"
+                width={0}
+                height={0}
+              />
+            </div>
+          </p>
+          <p className="text-dr-text font-bold text-base capitalize flex items-center gap-2">
+            <span>back end:</span>
+            <div className="flex gap-2">
+              <Image
+                className="w-6 h-6"
+                src={express}
+                alt="html"
+                width={0}
+                height={0}
+              />
+              <Image
+                className="w-6 h-6"
+                src={tailwind}
+                alt="html"
+                width={0}
+                height={0}
+              />
+              <Image
+                className="w-6 h-6"
+                src={mongoose}
+                alt="html"
+                width={0}
+                height={0}
+              />
+            </div>
+          </p>
+        </div>
+      </motion.div>
       <div className="w-full h-full flex justify-center items-end">
-        <h2 className="text-white font-bold text-lg">Hello</h2>
+        <h2 className="text-dr-text font-bold text-lg pb-2 capitalize dark:text-lg-text">
+          tvs - shop website
+        </h2>
       </div>
     </motion.div>
   );
