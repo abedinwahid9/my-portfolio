d3c5f2bef6ecc0b8f2b2f0e879d7a28e9a84dac3
abedin wahid <abedinwahid9@gmail.com>
2025-02-01 02:46:42 +0600

    skills page design and reuseable components coplete

--- src/app/(home)/skills/page.tsx
+++ src/app/(home)/skills/page.tsx
@@ -4,8 +4,48 @@ import SkillCard from "@/app/components/share/SkillCard";
 import Title from "@/app/components/share/Title";
 import ArrowSvgAni from "@/app/components/svg/ArrowSvgAni";
 import React from "react";
+import html from "../../../../public/icons/icons8-html-480.png";
+import css from "../../../../public/icons/icons8-css3-480.png";
+import js from "../../../../public/icons/icons8-javascript-480.png";
+import bootstrap from "../../../../public/icons/icons8-bootstrap-480.png";
+import tailwind from "../../../../public/icons/icons8-tailwindcss-480.png";
+import express from "../../../../public/icons/icons8-express-js-500.png";
+import figma from "../../../../public/icons/icons8-figma-480.png";
+import photoshop from "../../../../public/icons/icons8-photoshop-480.png";
+import npm from "../../../../public/icons/icons8-npm-480.png";
 
 const page = () => {
+  const skills = [
+    {
+      category: "front end technology",
+      skills: [
+        { title: "html", icon: html },
+        { title: "css", icon: css },
+        { title: "bootstrap", icon: bootstrap },
+        { title: "tailwind", icon: tailwind },
+      ],
+    },
+    {
+      category: "programming language",
+      skills: [{ title: "javascript", icon: js }],
+    },
+    {
+      category: "back end technology",
+      skills: [{ title: "express js", icon: express }],
+    },
+    {
+      category: "ui ux",
+      skills: [
+        { title: "figma", icon: figma },
+        { title: "photo shop", icon: photoshop },
+      ],
+    },
+    {
+      category: "others",
+      skills: [{ title: "npm", icon: npm }],
+    },
+  ];
+
   return (
     <div className=" py-10 md:px-5 px-3">
       <section className="flex md:flex-row flex-col gap-3">
@@ -33,14 +73,22 @@ const page = () => {
         <AnimeDivider />
       </div>
 
-      <section className="py-3">
-        <div className="md:w-2/3 w-full">
-          <Title title="front end technology" />
-        </div>
-        <div className="py-5">
-          <SkillCard />
-        </div>
-      </section>
+      {skills?.map((cate, i) => {
+        return (
+          <section key={i} className="py-3">
+            <div className="md:w-2/3 w-full lg:ml-4 ml-3">
+              <Title title={`${cate.category} :`} />
+            </div>
+            <div className="py-4 grid lg:grid-cols-8 md:grid-cols-6 grid-cols-3 justify-center place-items-center items-center gap-3">
+              {cate.skills?.map((skill, i) => {
+                return (
+                  <SkillCard key={i} title={skill.title} img={skill.icon} />
+                );
+              })}
+            </div>
+          </section>
+        );
+      })}
     </div>
   );
 };
--- src/app/components/share/SkillCard.tsx
+++ src/app/components/share/SkillCard.tsx
@@ -1,24 +1,50 @@
 "use client";
 import { motion } from "framer-motion";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import { FC } from "react";
-import js from "../../../../public/icons/icons8-javascript-480.png";
+import { shadowVariants } from "../animation/animatedBtn";
 
-const SkillCard: FC = () => {
+interface SKILLCARD {
+  title: string;
+  img: string | StaticImageData;
+  key: number;
+}
+
+const SkillCard: FC<SKILLCARD> = ({ title, img, key }) => {
   return (
-    <motion.div className="w-20 h-20 border-2 border-lg-link flex justify-center items-center flex-col relative overflow-hidden group">
-      <div className="w-10 h-10 relative z-10">
+    <motion.div
+      key={key}
+      className="md:w-24 w-20 md:h-24 h-20 shadow-[inset_0px_0px_10px_5px_#1E88A8] drop-shadow-2xl flex justify-center items-center flex-col relative overflow-hidden group rounded-2xl"
+    >
+      <div className="md:w-10 w-8 md:h-10 h-8 relative z-10">
         <Image
           className="w-full h-full"
-          width={0}
-          height={0}
-          src={js || "/placeholder.svg"}
-          alt="js"
+          width={40} // Set appropriate width
+          height={40} // Set appropriate height
+          src={img}
+          alt={title}
         />
       </div>
-      <p className="select-none relative z-10">javascript</p>
-      {/* shine overlay */}
-      <div className="absolute top-0 left-[-100%] w-full h-full bg-gradient-to-r from-transparent via-white/20 to-transparent transform skew-x-12 transition-all duration-700 ease-in group-hover:left-full" />
+
+      {/* Static shadow effect */}
+      <motion.div
+        variants={shadowVariants}
+        initial="stop"
+        animate="start"
+        style={{
+          background:
+            "radial-gradient(circle, rgba(80,170,184,1) 10%, rgba(80,170,184,0.76) 20%, rgba(80,170,184,0.01) 50%)",
+          filter: "blur(3px)",
+        }}
+        className="w-full h-[6px] rounded-sm"
+      ></motion.div>
+
+      <p className="select-none relative z-10 capitalize font-semibold dark:text-lg-text text-dr-text text-xs md:text-base">
+        {title}
+      </p>
+
+      {/* Shine overlay */}
+      <div className="absolute top-0 left-[-100%] w-full h-full bg-gradient-to-r from-transparent dark:via-lg-link/40 via-lg-text/60 to-transparent transform skew-x-12 transition-all duration-700 ease-in group-hover:left-full" />
     </motion.div>
   );
 };
